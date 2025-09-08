ddbfc77134b7f4601f5e56b65f0004810f2424a8
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-08 09:54:05 -0400

    Print formatting options

--- rust_linkedin_learning_tutorials/module_2_code_samples/rust_data_types/src/main.rs
+++ rust_linkedin_learning_tutorials/module_2_code_samples/rust_data_types/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let a = 10; 
+    let a = 10.0; 
     let b = 3.0; 
-    let c = a as f64 / (b + 1.0); 
-    println!("c is {}", c);
-   
+    let c = a / b; 
+    println!("c is {:08.3}\na is {}", c, a);
+    println!("c is {:08.3}\na is {}", c, a);
+
 }
