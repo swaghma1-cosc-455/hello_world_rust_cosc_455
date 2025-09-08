5a7dc60086df1b118a9dacf7280a5eacc4cb623f
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-08 13:32:05 -0400

    Tuples in Rust

--- rust_linkedin_learning_tutorials/module_3_code_samples/compound_data_types/src/main.rs
+++ rust_linkedin_learning_tutorials/module_3_code_samples/compound_data_types/src/main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    let parking_lot = [[1, 2, 3], 
-                       [4, 5, 6]];
+    let mut stuff: (u8, f32, char) = (10, 3.14, 'x'); 
+    stuff.0 += 3; 
+    let first_item = stuff.0; 
+    println!("first_item is {}", first_item); 
 
-    let number = parking_lot[1][2]; 
-
-    println!("number is {}", number); 
-
-    let garage: [[[0; 100] ; 20]; 5];  
-
-    
+    let (a, b, c) = stuff; 
+    println!("b is {}", b); 
 }
