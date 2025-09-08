6fb943f63648131184afc05885ddd8f7ad34b2df
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-08 12:32:14 -0400

    Explored binary operators and data types

--- rust_linkedin_learning_tutorials/module_2_code_samples/rust_data_types/src/main.rs
+++ rust_linkedin_learning_tutorials/module_2_code_samples/rust_data_types/src/main.rs
@@ -1,27 +1,13 @@
 fn main() {
-    let mut value = 0b1111_0101u8; 
-    println!("value is {}", value); 
-    println!("value is {:08b}", value); 
+    let a = true; 
+    let b = false; 
 
-    //Using the NOT operator
-    value = !value; 
-    println!("value is {:08b}", value); 
+    println!("a is {} and b is {}", a, b); 
+    println!("NOT a is {}", !a); 
+    println!("a AND b is {}", a & b);
+    println!("a OR b is {}", a | b); 
+    println!("a XOR b is {}", a ^ b); 
 
-    //Using the AND operator
-    value = value & 0b1111_0111; 
-    println!("value is {:08b}", value);
-    //Another use of AND operator
-    println!("bit 6 is {}", value & 0b0100_0000); 
-
-    value = value | 0b0100_0000; 
-    println!("value is {:08b}", value); 
-
-    value = value ^ 0b0101_0101; 
-    println!("value is {:08b}", value); 
-
-    value = value << 4; 
-    println!("value is {:08b}", value); 
-
-    value = value >> 2; 
-    println!("value is {:08b}", value); 
+    let c = (a ^ b) || panic!(); 
+    println!("c is {}", c); 
 }
