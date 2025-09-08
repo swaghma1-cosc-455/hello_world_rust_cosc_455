f2cbca4c690bc8e9d0b0f4c72eadaeb8b8eb7cfa
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-08 12:23:30 -0400

    Explored bitwise operators

--- rust_linkedin_learning_tutorials/module_2_code_samples/rust_data_types/src/main.rs
+++ rust_linkedin_learning_tutorials/module_2_code_samples/rust_data_types/src/main.rs
@@ -1,7 +1,27 @@
 fn main() {
-    let a = 10.0; 
-    let b = 3.0; 
-    let c = a / b; 
-    print!("c is {0:08.3}\na is {1}\n once again, c is {0}", c, a);
+    let mut value = 0b1111_0101u8; 
+    println!("value is {}", value); 
+    println!("value is {:08b}", value); 
 
+    //Using the NOT operator
+    value = !value; 
+    println!("value is {:08b}", value); 
+
+    //Using the AND operator
+    value = value & 0b1111_0111; 
+    println!("value is {:08b}", value);
+    //Another use of AND operator
+    println!("bit 6 is {}", value & 0b0100_0000); 
+
+    value = value | 0b0100_0000; 
+    println!("value is {:08b}", value); 
+
+    value = value ^ 0b0101_0101; 
+    println!("value is {:08b}", value); 
+
+    value = value << 4; 
+    println!("value is {:08b}", value); 
+
+    value = value >> 2; 
+    println!("value is {:08b}", value); 
 }
