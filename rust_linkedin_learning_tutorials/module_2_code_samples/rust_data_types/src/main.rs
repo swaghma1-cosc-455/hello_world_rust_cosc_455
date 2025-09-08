ec300a7d249b0eb2aea0cc2fcb0ce06fd79dbad4
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-08 12:07:27 -0400

    positioning variables

--- rust_linkedin_learning_tutorials/module_2_code_samples/rust_data_types/src/main.rs
+++ rust_linkedin_learning_tutorials/module_2_code_samples/rust_data_types/src/main.rs
@@ -2,7 +2,6 @@ fn main() {
     let a = 10.0; 
     let b = 3.0; 
     let c = a / b; 
-    println!("c is {:08.3}\na is {}", c, a);
-    println!("c is {:08.3}\na is {}", c, a);
+    print!("c is {0:08.3}\na is {1}\n once again, c is {0}", c, a);
 
 }
