123bf521ca3fc1b24b27bf684f7f2648b6e33df2
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-08 13:18:06 -0400

    Arrays in Rust

---
+++ rust_linkedin_learning_tutorials/module_3_code_samples/compound_data_types/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut letters = ['a', 'b', 'c']; 
+    letters[0] = 'x'; 
+    let first_letter = letters[0]; 
+    println!("first_letter is {}", first_letter); 
+
+    let numbers: [i32; 5]; 
+    numbers = [0; 5]; 
+    let index: usize = numbers.len() - 1; 
+    println!("last number is {}", numbers[index]); 
+    
+}
