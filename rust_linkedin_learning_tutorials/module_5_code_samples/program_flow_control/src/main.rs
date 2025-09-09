0ccaade5496e579237b4073d0072407bcf2f2691
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-09 05:28:03 -0400

    Nested if else statements

--- rust_linkedin_learning_tutorials/module_5_code_samples/program_flow_control/src/main.rs
+++ rust_linkedin_learning_tutorials/module_5_code_samples/program_flow_control/src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    let x = 4; 
+    let x = 3; 
+    let y = 5; 
 
-    if x + 1 != 3{
-        println!("x + 1 is NOT 3!"); 
+    if x > y {
+        println!("x is greater than y"); 
     }
+    else if x < y {
+            println!("x is less than y"); 
+    } else {
+            println!("x is equal to y"); 
+    }
+    
 }
