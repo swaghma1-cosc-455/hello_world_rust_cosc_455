92e8376985296192166b93a7dcd4134b9e4c5614
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-09 05:34:03 -0400

    Shorthand operator

--- rust_linkedin_learning_tutorials/module_5_code_samples/program_flow_control/src/main.rs
+++ rust_linkedin_learning_tutorials/module_5_code_samples/program_flow_control/src/main.rs
@@ -1,14 +1,12 @@
 fn main() {
-    let x = 3; 
-    let y = 5; 
+    let make_x_odd = true; 
+    let x = if make_x_odd {1} else {2};
 
-    if x > y {
-        println!("x is greater than y"); 
-    }
-    else if x < y {
-            println!("x is less than y"); 
-    } else {
-            println!("x is equal to y"); 
-    }
-    
+    // if make_x_odd {
+    //     x = 1; 
+    // } else {
+    //     //x = 2; 
+    // }
+
+    println!("x is {}", x); 
 }
