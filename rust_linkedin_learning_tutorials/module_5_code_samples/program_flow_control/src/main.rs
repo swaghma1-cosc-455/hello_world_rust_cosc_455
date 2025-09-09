423f37f73b2e9dec72cb78c3f8d7447df42ea8d5
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-09 06:01:30 -0400

    nested loops

--- rust_linkedin_learning_tutorials/module_5_code_samples/program_flow_control/src/main.rs
+++ rust_linkedin_learning_tutorials/module_5_code_samples/program_flow_control/src/main.rs
@@ -1,15 +1,11 @@
 fn main() {
-    let message = ['h', 'e', 'l', 'l', 'o']; 
+    let mut matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
-    for (index, &item) in message.iter().enumerate() {
-        println!("item {} is {}", index, item); 
-        if item == 'e' {
-            break; 
+    for row in matrix.iter_mut() {
+        for num in row.iter_mut(){
+            *num += 10; 
+            print!("{}\t", num); 
         }
-    }
-
-    for number in 0..5{
-        println!("number is {}", number); 
-    }
-    
+        println!(); 
+    } 
 }
