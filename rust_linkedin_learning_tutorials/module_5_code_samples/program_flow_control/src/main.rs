f876aceb2b4e0af3bc1e002fc47ea997bcee693f
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-09 05:47:55 -0400

    while loops

--- rust_linkedin_learning_tutorials/module_5_code_samples/program_flow_control/src/main.rs
+++ rust_linkedin_learning_tutorials/module_5_code_samples/program_flow_control/src/main.rs
@@ -1,16 +1,10 @@
 fn main() {
     let mut count = 0; 
+    let letters = ['a', 'b', 'c']; 
 
-    let result = loop {
-        if count == 10 {
-            break count * 10; 
-        }
-        count += 1; 
-        println!("count is {}", count); 
-    }; 
-
-    println!("After the loop!");
-    println!("result is {}", result);
-
+    while count < letters.len() {
+        println!("letter is {}", letters[count]); 
+        count +=1 ; 
+    }
     
 }
