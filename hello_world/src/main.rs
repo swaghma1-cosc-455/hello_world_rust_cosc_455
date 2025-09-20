8edb2b4fa042c8aaeedb7d37092f06c0813ac791
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-20 16:16:59 -0400

    First program of input/output

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::io; 
+
+fn main(){
+    let mut buffer = String::new(); 
+    println!("Enter a message: "); 
+    io::stdin().read_line(&mut buffer); 
+    println!("buffer is {}", buffer); 
 }
