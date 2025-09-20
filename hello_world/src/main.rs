a7a28152086a27db72a432cd6c145a2e04b2f81b
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-20 17:00:24 -0400

    Testing random crate functionality

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,12 +1,20 @@
-use std::io; 
+use core::num;
+
+use rand::prelude::*; 
 
 fn main(){
-    let mut buffer = String::new(); 
-    println!("Enter a number: "); 
-    io::stdin().read_line(&mut buffer); 
-    println!("buffer is {}", buffer); 
+    // let mut buffer = String::new(); 
+    // println!("Enter a number: "); 
+    // io::stdin().read_line(&mut buffer); 
+    // println!("buffer is {}", buffer); 
 
-    let number: i32 = buffer.trim().parse().unwrap();
+    // let number: i32 = buffer.trim().parse().unwrap();
 
-    println!("number+1 is {}", number);  
+    // println!("number+1 is {}", number);  
+
+    let number = random::<f64>(); 
+    println!("number is {}", number); 
+
+    let number = thread_rng().gen_range(1..11); 
+    println!("number is {}", number); 
 }
