334e946261cbb52a622cc56fb8e42c5d87f1b9c2
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-20 16:57:58 -0400

    Getting an input

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -2,7 +2,11 @@ use std::io;
 
 fn main(){
     let mut buffer = String::new(); 
-    println!("Enter a message: "); 
+    println!("Enter a number: "); 
     io::stdin().read_line(&mut buffer); 
     println!("buffer is {}", buffer); 
+
+    let number: i32 = buffer.trim().parse().unwrap();
+
+    println!("number+1 is {}", number);  
 }
