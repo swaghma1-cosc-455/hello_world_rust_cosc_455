5f7fa32be07616afe5d9f12dc23395e11f8a44d6
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-20 19:01:42 -0400

    Explored read and write options for files

--- rust_linkedin_learning_tutorials/module_9_code_samples/rust_io_exercises/src/main.rs
+++ rust_linkedin_learning_tutorials/module_9_code_samples/rust_io_exercises/src/main.rs
@@ -1,14 +1,18 @@
 use std::fs; 
+use std::io::prelude::*; 
 fn main() {
-    let contents = fs::read_to_string("planets.txt").unwrap();
-    println!("contents is {}", contents ); 
+    let mut speech = String::new(); 
+    speech.push_str("We choose to go to the Moon in this decade\n"); 
+    speech.push_str("and do the other things, \n"); 
+    speech.push_str("not because they are easy, \n"); 
+    speech.push_str("but because they are hard. "); 
 
-    for line in contents.lines()
-    {
-        println!("line is {}", line); 
-    }
+    fs::write("speech.txt", speech); 
 
-    let contents = fs::read("planets.txt").unwrap();
-    println!("contents is {:?}", contents ); 
+    let mut file = fs::OpenOptions::new().append(true).open("planets.txt").unwrap();
+    file.write(b"\nPluto"); 
+
+
+
 
 }
