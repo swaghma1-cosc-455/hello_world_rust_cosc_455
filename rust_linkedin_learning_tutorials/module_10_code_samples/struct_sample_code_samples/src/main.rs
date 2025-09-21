9d55e1944b27d46ca741b3dc62490758a157b9c4
tiger-dev-hacker <waghmaresuril@gmail.com>
2025-09-20 20:34:54 -0400

    Tuple structs

--- rust_linkedin_learning_tutorials/module_10_code_samples/struct_sample_code_samples/src/main.rs
+++ rust_linkedin_learning_tutorials/module_10_code_samples/struct_sample_code_samples/src/main.rs
@@ -1,40 +1,14 @@
+struct Color(u8, u8, u8); //RGB
+struct Point(u8, u8, u8); //XYZ
 
-struct Shuttle{
-    name: String, 
-    crew_size: u8, 
-    propellant: f64, 
+fn get_y(p: Point) -> u8{
+    p.1
 }
+fn main(){
+    let red = Color(255, 0, 0); 
+    println!("First value is {}", red.0); 
 
-impl Shuttle {
-    fn get_name(&self) -> &str {
-        &self.name
-    }
-
-    fn add_fuel(&mut self, gallons: f64) {
-        self.propellant += gallons; 
-    }
-
-    fn new(name: &str) -> Shuttle {
-        Shuttle {
-            name: String::from(name), 
-            crew_size: 7, 
-            propellant: 0.0
-        }
-    }
-}
-
-
-fn main() {
-    let mut vehicle = Shuttle::new("Endeavour"); 
-    let mut vehicle2 = Shuttle::new("Discovery"); 
-
-    let vehicle_name = vehicle.get_name(); 
-    println!("vehicle_name is {}", vehicle_name); 
-
-    println!("propellant is {}", vehicle.propellant);
-
-    vehicle.add_fuel(1000.0); 
-
-    println!("propellant is {}", vehicle.propellant);
-
+    let coord = Point(4, 5, 6); 
+    let y = get_y(coord); 
+    println!("y is {}", y); 
 }
